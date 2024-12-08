136ffb5fc78ba19dcc20b0ea94fdfb19a1993da8
Mario Yordanov <mariovyordanov@gmail.com>
2024-12-08 10:55:40 +0200

    render sharp pixel art

--- scripts/core/canvas.js
+++ scripts/core/canvas.js
@@ -3,3 +3,9 @@ export const ctx = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
+
+// Render pixel art without smoothing
+// Source: https://html.spec.whatwg.org/multipage/canvas.html#image-smoothing
+ctx.imageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+ctx.webkitImageSmoothingEnabled = false;
